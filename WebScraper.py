800c0a1b57c84d577fe4fbf001792d4c189facb5
sherief nabeeh <shereff@ymail.com>
2020-05-25 23:39:57 +0200

    slight amendments comments

--- WebScraper.py
+++ WebScraper.py
@@ -1,27 +1,31 @@
-import requests, bs4, math, time
+import bs4
+import time
 
-pageNo = 1 
+from pip._vendor import requests
+
+pageNo = 1
 arrayOfCars = []
 counterEndFlag = 0
 endFlag = 1
 
-while(endFlag):
-    print('Requesting page in 5 seconds...')
+while endFlag:
+    print('Requesting page in 5 seconds..')
     time.sleep(5)
-    webstr = 'https://www.contactcars.com/usedcars?search=uc&mk=6&md=19&page='+ str(pageNo)
-    print(webstr)
+    webString = 'https://www.contactcars.com/usedcars?search=uc&mk=6&md=19&page=' + str(pageNo)
+    print(webString)
 
-    res = requests.get(webstr)
+    res = requests.get(webString)
     res.raise_for_status()
     testFileBS = bs4.BeautifulSoup(res.text, 'html.parser')
-
+    open('parsedHTML.html' 'wb').write(res.content)
     for i in testFileBS.select('div[carid]'):
-        arrayOfData = [int(i.find('input')['data-id']), int(i.find('input')['data-price']), int(i.find('input')['data-year'])]
+        arrayOfData = [int(i.find('input')['data-id']),
+                       int(i.find('input')['data-price']), int(i.find('input')['data-year'])]
         arrayOfCars.append(arrayOfData)
         counterEndFlag += 1
 
-    print("Number of cars found in page "+ str(pageNo) + ": " + str(counterEndFlag) + " car(s)")
-    if(counterEndFlag == 0):
+    print("Number of cars found in page " + str(pageNo) + ": " + str(counterEndFlag) + " car(s)")
+    if counterEndFlag == 0:
         print('All car ads have been collected.\n')
         endFlag = 0
     counterEndFlag = 0
@@ -29,7 +33,7 @@ while(endFlag):
 
 print(arrayOfCars)
 
-### TODO ##
+# Todo
 # Since ads are sorted by date of creation, to minimize unnecessary collection of data
 # we can compare the carid in the for loop to the latest car id stored in our database
 # and end data collection when a match is found. But first we need to find a way to

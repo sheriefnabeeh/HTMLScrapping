2f8d340bca59e2ee160e1413a41a3a79e0d28b15
Momen <momen@localpc.com>
2020-05-25 05:46:52 +0200

    removing unneeded files, script is now fetches data from the website

--- WebScraper.py
+++ WebScraper.py
@@ -1,7 +1,36 @@
-import requests, bs4, math
-# BMW 320i
-testFile = open('testcontactcars.html', encoding='utf8')
-testFileBS = bs4.BeautifulSoup(testFile, 'html.parser')
+import requests, bs4, math, time
 
-for i in testFileBS.select('div[carid]'):
-    print([int(i.find('input')['data-id']), int(i.find('input')['data-price']), int(i.find('input')['data-year'])])
+pageNo = 1 
+arrayOfCars = []
+counterEndFlag = 0
+endFlag = 1
+
+while(endFlag):
+    print('Requesting page in 5 seconds...')
+    time.sleep(5)
+    webstr = 'https://www.contactcars.com/usedcars?search=uc&mk=6&md=19&page='+ str(pageNo)
+    print(webstr)
+
+    res = requests.get(webstr)
+    res.raise_for_status()
+    testFileBS = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    for i in testFileBS.select('div[carid]'):
+        arrayOfData = [int(i.find('input')['data-id']), int(i.find('input')['data-price']), int(i.find('input')['data-year'])]
+        arrayOfCars.append(arrayOfData)
+        counterEndFlag += 1
+
+    print("Number of cars found in page "+ str(pageNo) + ": " + str(counterEndFlag) + " car(s)")
+    if(counterEndFlag == 0):
+        print('All car ads have been collected.\n')
+        endFlag = 0
+    counterEndFlag = 0
+    pageNo += 1
+
+print(arrayOfCars)
+
+### TODO ##
+# Since ads are sorted by date of creation, to minimize unnecessary collection of data
+# we can compare the carid in the for loop to the latest car id stored in our database
+# and end data collection when a match is found. But first we need to find a way to
+# store our data

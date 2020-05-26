2a329dd84b5ed7b6dcca2a62d0cda9add4173616
Momen <momen@localpc.com>
2020-05-27 00:39:09 +0200

    minor changes before integration and reorganization

--- WebScraper.py
+++ WebScraper.py
@@ -1,34 +1,36 @@
-import requests, bs4, math, time
+import requests, bs4, time
 
-pageNo = 1 
-arrayOfCars = []
-counterEndFlag = 0
-endFlag = 1
-
-while(endFlag):
-    print('Requesting page in 5 seconds...')
-    time.sleep(5)
-    webstr = 'https://www.contactcars.com/usedcars?search=uc&mk=6&md=19&page='+ str(pageNo)
-    print(webstr)
-
-    res = requests.get(webstr)
-    res.raise_for_status()
-    testFileBS = bs4.BeautifulSoup(res.text, 'html.parser')
-
-    for i in testFileBS.select('div[carid]'):
-        arrayOfData = [int(i.find('input')['data-id']), int(i.find('input')['data-price']), int(i.find('input')['data-year'])]
-        arrayOfCars.append(arrayOfData)
-        counterEndFlag += 1
-
-    print("Number of cars found in page "+ str(pageNo) + ": " + str(counterEndFlag) + " car(s)")
-    if(counterEndFlag == 0):
-        print('All car ads have been collected.\n')
-        endFlag = 0
+def ReadDataFromContactCars(carmake, carmodel):
+    pageNo = 1 
+    arrayOfCars = []
     counterEndFlag = 0
-    pageNo += 1
+    endFlag = 1
 
-print(arrayOfCars)
+    while(endFlag):
+        print('Requesting page in 5 seconds...')
+        time.sleep(5)
+        webstr = 'https://www.contactcars.com/usedcars?search=uc&mk=' + str(carmake) + '&md=' + str(carmodel) + '&page='+ str(pageNo)
+        print(webstr)
 
+        res = requests.get(webstr)
+        res.raise_for_status()
+        testFileBS = bs4.BeautifulSoup(res.text, 'html.parser')
+
+        for i in testFileBS.select('div[carid]'):
+            arrayOfData = [int(i.find('input')['data-id']), int(i.find('input')['data-price']), int(i.find('input')['data-year'])]
+            arrayOfCars.append(arrayOfData)
+            counterEndFlag += 1
+
+        print("Number of cars found in page "+ str(pageNo) + ": " + str(counterEndFlag) + " car(s)")
+        if(counterEndFlag == 0):
+            print('All car ads have been collected.\n\n')
+            endFlag = 0
+        counterEndFlag = 0
+        pageNo += 1
+    
+    return arrayOfCars
+
+print(ReadDataFromContactCars(6,19))
 ### TODO ##
 # Since ads are sorted by date of creation, to minimize unnecessary collection of data
 # we can compare the carid in the for loop to the latest car id stored in our database
---
+++ carMap.py
@@ -0,0 +1,33 @@
+import json
+from bs4 import BeautifulSoup
+
+# get content from html carMakes
+file_CarMakes = open('CarMakes.html',encoding='utf8')
+parsed = BeautifulSoup(file_CarMakes, 'html.parser')
+# print(parsed.find_all('option'))
+# initializing variables for loop and dictionary
+dict_CarMakes = {}
+i = 0
+for single in parsed.findAll('option'):
+    if i == 0:
+        i += 1
+        continue
+    # dict_CarMakes.update({single.get('value'): single.get_text()})
+    dict_CarMakes[single.get('value')] = single.get_text()
+
+print(dict_CarMakes)
+
+def exportjson(dict_CarMakes):
+    with open("carMakesDict.json", 'w', encoding="utf-8") as outfile:
+        json.dump(dict_CarMakes, outfile)
+    return
+
+
+def importjson(infilename):
+    with open("carMakesDict.json", 'r', encoding="utf-8") as infile:
+      data =  json.load(infile)
+      print(data)
+    return
+
+exportjson(dict_CarMakes)
+importjson("carMakesDict.json")
